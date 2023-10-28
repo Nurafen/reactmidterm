9438c1780eba21c28855a9de0515174fcf5f7d92
Nurafen <98904187+Nurafen@users.noreply.github.com>
2023-10-28 15:09:49 +0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'rct'
+import ReactDOM from 'rct-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
